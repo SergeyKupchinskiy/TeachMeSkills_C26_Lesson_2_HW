3643e027f846a892440bd13c9fa6530a76a5c027
SergeyKupchinskiy <149712939+SergeyKupchinskiy@users.noreply.github.com>
2023-11-05 21:08:45 +0300

    Update Ex1.java

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ex1 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Введите B");
         Scanner scan = new Scanner(System.in);
         int B = scan.nextInt();
